9acd8d38a25d43c757b993bf94da3b658d352ccd
alyson <consulialyson@gmail.com>
2022-10-30 17:38:51 -0300

    feat: class Bank Account with deposit method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(){
+        BankAccount account1 = new BankAccount("Alyson", 800);
+
+        Console.WriteLine(account1.Balance);
+        account1.Deposit(100);
+        Console.WriteLine(account1.Balance);
+    }
+}
+
+class BankAccount{
+    private string name;
+    private decimal balance;
+
+    public decimal Balance{
+        get { return balance; }
+    }
+
+    public BankAccount(string name, decimal balance){
+        if(string.IsNullOrWhiteSpace(name)){
+            throw new ArgumentException("Invalid name.", nameof(name));
+        }
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void Deposit(decimal amount){
+        if(amount <= 0){
+            throw new ArgumentException("You cannot deposit numbers less than or equal to 0.", nameof(amount));
+        }
+        balance += amount;
+    }
+}
